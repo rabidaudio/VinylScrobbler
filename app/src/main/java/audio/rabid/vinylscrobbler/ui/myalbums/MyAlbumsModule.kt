ec560986ff4ce693fac95aea320f88a70de58925
Charles Julian Knight <julian@fixdapp.com>
2019-11-09 22:10:41 -0500

    fixup gradle

--- app/src/main/java/audio/rabid/vinylscrobbler/ui/myalbums/MyAlbumsModule.kt
+++ app/src/main/java/audio/rabid/vinylscrobbler/ui/myalbums/MyAlbumsModule.kt
@@ -1,18 +1,8 @@
 package audio.rabid.vinylscrobbler.ui.myalbums
 
-import audio.rabid.vinylscrobbler.core.ViewModel
-import toothpick.ktp.binding.bind
 import toothpick.ktp.binding.module
-import javax.inject.Provider
 
 val MyAlbumsModule = module {
 
-    bind<MyAlbumsViewModel>().toProvider()
-}
-
-class ViewModelProvider<VM : ViewModel> : Provider<VM> {
-
-    override fun get(): VM {
-
-    }
+//    bind<MyAlbumsViewModel>().toProvider()
 }
