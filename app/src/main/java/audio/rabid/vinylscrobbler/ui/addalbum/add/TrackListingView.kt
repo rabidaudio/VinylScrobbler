5de73368e759c93e3d76dbb61ec2e46d52092073
Charles Julian Knight <julian@fixdapp.com>
2020-03-25 23:02:07 -0400

    localize track listing view

--- app/src/main/java/audio/rabid/vinylscrobbler/ui/addalbum/add/TrackListingView.kt
+++ app/src/main/java/audio/rabid/vinylscrobbler/ui/addalbum/add/TrackListingView.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.widget.LinearLayout
+import audio.rabid.vinylscrobbler.R
 import audio.rabid.vinylscrobbler.databinding.ItemTrackBinding
 import java.time.Duration
 
@@ -21,7 +22,7 @@ class TrackListingView @JvmOverloads constructor(
         private val SAMPLE_DATA = listOf(
             Track("A1", "Too Small for Eyes", Duration.ofSeconds(347)),
             Track("A2", "It Hurts Until It Doesn’t", Duration.ofSeconds(334)),
-            Track("A3", "Copper Mines", Duration.ofSeconds(237)),
+            Track("A3", "Copper Mines", Duration.ofSeconds(837)),
             Track("A4", "Nesting Behavior", Duration.ofSeconds(377)),
             Track("B1", "Lockjaw", Duration.ofSeconds(319)),
             Track("B2", "Blood‐letting", Duration.ofSeconds(337)),
@@ -55,9 +56,12 @@ class TrackListingView @JvmOverloads constructor(
 }
 
 fun Duration.getLocalizedString(context: Context): String {
-    // TODO: this isn't localized...
     val h = toHours()
     val m = minusHours(toHours()).toMinutes()
     val s = minusHours(toHours()).let { it.minusMinutes(it.toMinutes()) }.seconds
-    return if (h == 0L) String.format("%d:%02d", m, s) else String.format("%d:%02d:%02d", h, m, s)
+    return if (h == 0L) {
+        context.getString(R.string.duration_format_standard, m, s)
+    } else {
+        context.getString(R.string.duration_format_hours, h, m, s)
+    }
 }
